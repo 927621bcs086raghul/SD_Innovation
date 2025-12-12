7d52b1298b5d101ec2420c056c785bcc94e796a0
927621bcs086raghul <raghulajay814@gmail.com>
2025-12-12 22:46:33 +0530

    created a commit message file with the following content:

--- src/component/home/Index.jsx
+++ src/component/home/Index.jsx
@@ -1,26 +1,94 @@
+import { SearchOutlined } from '@ant-design/icons';
+import { Input, Layout, Skeleton, Typography } from 'antd';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPopularMoviesStart } from './homeSlice';
+import './Index.css';
+const { Header, Content, Footer } = Layout
+const { Title, Text } = Typography
 
-const Index = () => {
-  const dispatch = useDispatch();
 
-  const { popularMovies } = useSelector((state) => state.home);
+function Index() {
+	const dispatch = useDispatch();
+
+  const { popularMovies, loading } = useSelector((state) => state.home);
+  console.log('Popular Movies:', popularMovies);
 
   useEffect(() => {
     dispatch(fetchPopularMoviesStart());
-  }, [dispatch]);
+  }, []);
 
   return (
-    <div>
-      <h2>Popular Movies</h2>
-          {popularMovies.map((movie) => (
-            <p key={movie.id}>
-              {movie.title}
-            </p>
-          ))}
-    </div>
-  );
-};
+    <Layout className="movies-page-root">
+      <Header className="movies-header">
+        <div className="movies-header-inner">
+          <div className="movies-header-left">
+            <div className="movies-logo">
+              <div className="movies-logo-icon" />
+              <Title level={4} className="movies-logo-title">
+                MovieStream
+              </Title>
+            </div>
+          </div>
+        </div>
+      </Header>
+
+            <Content className="movies-main">
+        <div className="movies-main-inner">
+          <div className="movies-heading-row">
+            <Title level={2} className="movies-page-title">
+              Popular Movies
+            </Title>
+            <div className="movies-search-wrapper">
+              <Input
+                prefix={<SearchOutlined />}
+                placeholder="Search movies, series..."
+                className="movies-search-input"
+              />
+            </div>
+          </div>
+
+          <div className="movies-grid">
+            {loading
+              ? Array.from({ length: 8 }).map((_, index) => (
+                  <article key={index} className="movies-card">
+                    <div className="movies-card-poster">
+                      <Skeleton avatar  active style={{ width: '100%', height: '100%' }} />
+                    </div>
+                    <div className="movies-card-body">
+                      <Skeleton
+                        avatar
+                        active
+                        title={{ width: '80%' }}
+                        paragraph={false}
+                      />
+                    </div>
+                  </article>
+                ))
+              : popularMovies.map((movie) => (
+                  <article key={movie.id} className="movies-card">
+                    <div className="movies-card-poster">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                      />
+                    </div>
+                    <div className="movies-card-body">
+                      <h3 className="movies-card-title">{movie.title}</h3>
+                      <p className="movies-card-meta">
+                        {movie.release_date
+                          ? new Date(movie.release_date).getFullYear()
+                          : 'N/A'}
+                      </p>
+                    </div>
+                  </article>
+                ))}
+          </div>
+        </div>
+      </Content>
+
+    </Layout>
+  )
+}
 
 export default Index;
--- src/redux/rootsaga.js
+++ src/redux/rootsaga.js
@@ -1,3 +1,4 @@
+import { message } from 'antd';
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import { fetchPopularMoviesApi } from '../api/apiconfig';
 import {
@@ -11,10 +12,13 @@ function* handleFetchPopularMovies() {
     const response = yield call(fetchPopularMoviesApi);
     const results = response?.data?.results || [];
     yield put(fetchPopularMoviesSuccess(results));
+    message.success('Popular movies loaded successfully');
   } catch (error) {
+    const errorMsg = error?.message || 'Failed to load popular movies';
     yield put(
-      fetchPopularMoviesFailure(error?.message || 'Failed to load popular movies')
+      fetchPopularMoviesFailure(errorMsg)
     );
+    message.error(errorMsg);
   }
 }
 
